80f2fcf88e199041fa3569d159b85389b25c94c3
Joey Petrone <joey.petrone@gmail.com>
2020-07-27 23:16:25 -0500

    Playing around with Regex

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CSharpBasics
 {
@@ -6,7 +8,32 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey(); // What happens if you run the app without this line?
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            var pattern = "[b-df-hj-np-tv-z]+";
+
+            foreach (var animal in animals)
+            {
+                var syllables = Regex.Split(animal, pattern, RegexOptions.IgnoreCase);
+
+                var combined = string.Concat(syllables);
+
+                Console.WriteLine(combined);
+
+                Console.WriteLine(syllables.Length);
+
+                foreach (var i in syllables)
+                {
+                    Console.WriteLine(i);
+                }
+
+                if (combined.Length == 2)
+                {
+                    Console.WriteLine(animal);
+                }
+            }
         }
     }
 }
