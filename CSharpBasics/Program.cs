0ac6ccfb8bb940f6ed86c46adb1ea691f87f5daa
Joey Petrone <joey.petrone@gmail.com>
2020-07-28 00:13:05 -0500

    Hello World complete

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -10,6 +11,8 @@ namespace CSharpBasics
         {
             Console.WriteLine("Howdy Y'all!!");
             Console.ReadKey(); // What happens if you run the app without this line?
+            Console.WriteLine(" ");
+            Console.WriteLine("Animals with two syllabols: ");
 
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
             var vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
@@ -35,8 +38,33 @@ namespace CSharpBasics
                 {
                     Console.WriteLine(animal);
                 }
-
             }
+
+            Console.WriteLine(" ");
+
+            var greetings = new string[] { "Howdy, How yall doing this fine morning?", "Good morning, now get to work", "Hey" };
+
+            Console.WriteLine("Please select a greeting: ");
+            for ( var i=0; i < greetings.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {greetings[i]}");
+            }
+
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                if (input != "1" && input != "2" && input != "3")
+                {
+                    Console.WriteLine($"Select a value 1 thru {greetings.Length}");
+                }
+
+            } while (input != "1" && input != "2" && input != "3");
+
+            var response = @$"You chose greeting {input}
+                             {greetings[int.Parse(input)-1]}";
+            Console.WriteLine(response);
         }
     }
 }
