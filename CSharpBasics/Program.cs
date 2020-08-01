cc02fb7f638338196c950ae267ca553a53579bbb
Joey Petrone <joey.petrone@gmail.com>
2020-07-31 21:10:11 -0500

    letter loops complete

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -12,7 +12,7 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-                                           //--- HELLO WORLD & CONSOLE INPUT ---//
+            //--- HELLO WORLD & CONSOLE INPUT ---//
 
             //Console.WriteLine("Howdy Y'all!!");
             //Console.ReadKey(); // What happens if you run the app without this line?
@@ -88,146 +88,180 @@ namespace CSharpBasics
             //                 {greetings[int.Parse(input) - 1]}";
             //Console.WriteLine(response);
 
-                                         //--- CALCULATOR ---//
+            //--- CALCULATOR ---//
 
-            Console.WriteLine(@"
-                                     ____       ___                 ____
-                                    /    \     /   \     |         /    \   
-                                    |         |_____|    |         |  
-                                    \____/    |     |    |____/    \____/
+            //    Console.WriteLine(@"
+            //                             ____       ___                 ____
+            //                            /    \     /   \     |         /    \   
+            //                            |         |_____|    |         |  
+            //                            \____/    |     |    |____/    \____/
 
 
-        This program allows you to calculate in different ways:
+            //This program allows you to calculate in different ways:
 
-            - Multiply a group of numbers by using the * operator ( * 1,2,3 would equal 6 )
-            - Square each number in the group by using the ^2 operator ( ^2 1,2,3 would equal 1,4,9 )
-            - Add the numbers in the group by ising the + operator ( + 1,2,3 would equal 6 )
-            - Divide the numbers in the group by using the / operator ( / 6,3 would equal 2 )
-            - Get the average of the group of numbers by using the avg operator ( avg 1,2,3 would equal 2 )
+            //    - Multiply a group of numbers by using the * operator ( * 1,2,3 would equal 6 )
+            //    - Square each number in the group by using the ^2 operator ( ^2 1,2,3 would equal 1,4,9 )
+            //    - Add the numbers in the group by ising the + operator ( + 1,2,3 would equal 6 )
+            //    - Divide the numbers in the group by using the / operator ( / 6,3 would equal 2 )
+            //    - Get the average of the group of numbers by using the avg operator ( avg 1,2,3 would equal 2 )
 
-                    ");
+            //            ");
 
-            string tryAgain = null;
+                string tryAgain = null;
+            //    do
+            //    {
+            //        Console.WriteLine("Please input the operator followed by a space and then 3 numbers 1-10 separated by commas:");
+            //        var numInput = Console.ReadLine();
+            //        Console.WriteLine(" ");
+
+            //        string operatorChoice = null;
+            //        string numberList = null;
+
+            //        // Check to see which kind of operator was used and save that operator to a variable
+            //        // Save the comma separated number list to a variable
+            //        if (numInput.Substring(0, 1) == "^")
+            //        {
+            //            operatorChoice = numInput.Substring(0, 2);
+            //            numberList = numInput.Substring(3);
+            //        }
+            //        else if (numInput.Substring(0, 3) == "avg")
+            //        {
+            //            operatorChoice = numInput.Substring(0, 3);
+            //            numberList = numInput.Substring(4);
+            //        } else if (numInput.Substring(0, 1) != "")
+            //        {
+            //            operatorChoice = numInput.Substring(0, 1);
+            //            numberList = numInput.Substring(2);
+            //        }
+
+
+            //        // Remove the commas in the number list
+            //        var numbersOnly = numberList.Split(",");
+
+            //        // Initiate a funcion based on it's operator
+            //        switch (operatorChoice)
+            //        {
+            //            case "*": /// Multiplication
+            //                var product = 1;
+
+            //                foreach (var n in numbersOnly)
+            //                {
+            //                    var multiplicand = int.Parse(n);
+
+            //                    product *= multiplicand;
+            //                }
+
+            //                Console.WriteLine($"The product of {numberList} is {product}");
+            //                break;
+
+            //            case "^2": // Square Root
+            //                StringBuilder squaredNumbers = new StringBuilder();
+
+            //                for (int i = 0; i < numbersOnly.Length; i++)
+            //                {
+            //                    int result = 1;
+
+            //                    var multiplicand = int.Parse(numbersOnly[i]);
+
+            //                    result = multiplicand * multiplicand;
+
+            //                    squaredNumbers.Append(result.ToString());
+
+            //                    if (i < numbersOnly.Length - 1)
+            //                    {
+            //                        squaredNumbers.Append(",");
+            //                    }
+            //                }
+
+            //                Console.WriteLine($"{numberList} squared is {squaredNumbers}");
+            //                break;
+            //            case "+": // Addition
+            //                var sum = 0;
+
+            //                foreach (var n in numbersOnly)
+            //                {
+            //                    var addend = int.Parse(n);
+
+            //                    sum += addend;
+            //                }
+
+            //                Console.WriteLine($"The sum of {numberList} is {sum}");
+            //                break;
+            //            case "/": // Division
+            //                var quotient = 1f;
+
+            //                for (int i = 0; i < numbersOnly.Length; i++)
+            //                {
+            //                    if (i == 0)
+            //                    {
+            //                        float dividend = float.Parse(numbersOnly[i]);
+            //                        float divisor = float.Parse(numbersOnly[i + 1]);
+
+            //                        quotient = dividend / divisor;
+            //                    }
+            //                    else if (i + 1 < numbersOnly.Length)
+            //                    {
+            //                        float divisor = float.Parse(numbersOnly[i + 1]);
+
+            //                        quotient /= divisor;
+            //                    }
+            //                }
+            //                Console.WriteLine($"The quotient of {numberList} is {quotient}");
+            //                break;
+            //            case "avg": // Average
+            //                var average = 1f;
+
+            //                var avgSum = 0f;
+
+            //                foreach (var n in numbersOnly)
+            //                {
+            //                    float avgAddend = int.Parse(n);
+
+            //                    avgSum += avgAddend;
+            //                }
+
+            //                average = avgSum / numbersOnly.Length;
+
+            //                Console.WriteLine($"The average of {numberList} is {average}");
+            //                break;
+            //            default: // Console error if incorrect operator
+            //                Console.WriteLine("ERROR: Incorrect operator");
+            //                break;
+            //        }
+
+            //        Console.WriteLine(" ");
+            //        Console.WriteLine("Would you like to try it again? (Y or N)");
+            //        tryAgain = Console.ReadLine();
+            //        Console.WriteLine(" ");
+            //    } while (tryAgain.ToLower() != "n");
+
+                                        // LETTER LOOPS //
+
+            Console.WriteLine("LETTER LOOPS!!");
+            Console.WriteLine(" ");
+
             do
             {
-                Console.WriteLine("Please input the operator followed by a space and then 3 numbers 1-10 separated by commas:");
-                var numInput = Console.ReadLine();
-                Console.WriteLine(" ");
+                Console.WriteLine("Enter your favorite lowercase or uppercase letters with no spaces inbetween: ");
+                var letterLoopInput = Console.ReadLine();
 
-                string operatorChoice = null;
-                string numberList = null;
+                var letterLoopOutput = "";
+                for (var i=0; i < letterLoopInput.Length; i++)
+                {
+                    letterLoopOutput += letterLoopInput[i].ToString().ToUpper();
 
-                // Check to see which kind of operator was used and save that operator to a variable
-                // Save the comma separated number list to a variable
-                if (numInput.Substring(0, 1) == "^")
-                {
-                    operatorChoice = numInput.Substring(0, 2);
-                    numberList = numInput.Substring(3);
-                }
-                else if (numInput.Substring(0, 3) == "avg")
-                {
-                    operatorChoice = numInput.Substring(0, 3);
-                    numberList = numInput.Substring(4);
-                } else if (numInput.Substring(0, 1) != "")
-                {
-                    operatorChoice = numInput.Substring(0, 1);
-                    numberList = numInput.Substring(2);
+                    for (var j=0; j < i; j++)
+                    {
+                        letterLoopOutput += letterLoopInput[i].ToString().ToLower();
+                    }
+
+                    if (i < letterLoopInput.Length - 1)
+                    {
+                        letterLoopOutput += "-";
+                    }
                 }
 
-
-                // Remove the commas in the number list
-                var numbersOnly = numberList.Split(",");
-
-                // Initiate a funcion based on it's operator
-                switch (operatorChoice)
-                {
-                    case "*": /// Multiplication
-                        var product = 1;
-
-                        foreach (var n in numbersOnly)
-                        {
-                            var multiplicand = int.Parse(n);
-
-                            product *= multiplicand;
-                        }
-
-                        Console.WriteLine($"The product of {numberList} is {product}");
-                        break;
-
-                    case "^2": // Square Root
-                        StringBuilder squaredNumbers = new StringBuilder();
-
-                        for (int i = 0; i < numbersOnly.Length; i++)
-                        {
-                            int result = 1;
-
-                            var multiplicand = int.Parse(numbersOnly[i]);
-
-                            result = multiplicand * multiplicand;
-
-                            squaredNumbers.Append(result.ToString());
-
-                            if (i < numbersOnly.Length - 1)
-                            {
-                                squaredNumbers.Append(",");
-                            }
-                        }
-
-                        Console.WriteLine($"{numberList} squared is {squaredNumbers}");
-                        break;
-                    case "+": // Addition
-                        var sum = 0;
-
-                        foreach (var n in numbersOnly)
-                        {
-                            var addend = int.Parse(n);
-
-                            sum += addend;
-                        }
-
-                        Console.WriteLine($"The sum of {numberList} is {sum}");
-                        break;
-                    case "/": // Division
-                        var quotient = 1f;
-
-                        for (int i = 0; i < numbersOnly.Length; i++)
-                        {
-                            if (i == 0)
-                            {
-                                float dividend = float.Parse(numbersOnly[i]);
-                                float divisor = float.Parse(numbersOnly[i + 1]);
-
-                                quotient = dividend / divisor;
-                            }
-                            else if (i + 1 < numbersOnly.Length)
-                            {
-                                float divisor = float.Parse(numbersOnly[i + 1]);
-
-                                quotient /= divisor;
-                            }
-                        }
-                        Console.WriteLine($"The quotient of {numberList} is {quotient}");
-                        break;
-                    case "avg": // Average
-                        var average = 1f;
-
-                        var avgSum = 0f;
-
-                        foreach (var n in numbersOnly)
-                        {
-                            float avgAddend = int.Parse(n);
-
-                            avgSum += avgAddend;
-                        }
-
-                        average = avgSum / numbersOnly.Length;
-
-                        Console.WriteLine($"The average of {numberList} is {average}");
-                        break;
-                    default: // Console error if incorrect operator
-                        Console.WriteLine("ERROR: Incorrect operator");
-                        break;
-                }
+                Console.WriteLine($"{letterLoopOutput}");
 
                 Console.WriteLine(" ");
                 Console.WriteLine("Would you like to try it again? (Y or N)");
